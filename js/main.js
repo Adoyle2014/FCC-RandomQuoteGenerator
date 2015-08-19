99e7280f31b5c7b2d7de2ca75791515e2b46210b
Adam Doyle <dominofoe@gmail.com>
2015-08-18 22:43:37 -0400

    ACAO error

--- js/main.js
+++ js/main.js
@@ -1,47 +1,42 @@
 $(document).ready(function() {
+   function corsRequest() {
+       $.ajax({
+           type: 'GET',
+           url: 'http://api.forismatic.com/api/1.0/',
+           contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+           xhrFields: {
+               method: "getQuote",
+               format: "jsonp",
+               lang: "en"
+           },
+           headers: {
+               'Access-Control-Allow-Origin': "*"
+           },
 
-    function getQuote() {
-        var params = {
-            method: 'getQuote',
-            format: 'jsonp',
-            lang: 'en',
-            jsonp: 'parseQuote'
-        };
-        var url = 'http://api.forismatic.com/api/1.0/?';
 
-        $.getJSON(url, params, function(data) {
-            parseQuote(data);
-            })
+           success: function (data) {
+               parseQuote(data);
+           },
+           error: function () {
+               document.getElementById("quote").innerHTML = "There was an error with your request.  Please try again.";
+           }
 
-    }
+       });
+   }
 
-    function parseQuote(response) {
+
+    function parseQuote(response)
+    {
+        console.log(response);
+        /*
         document.getElementById("quote").innerHTML = response.quoteText;
         document.getElementById("author").innerHTML = response.quoteAuthor;
+        */
     }
 
-    $("#newQuote").on('click', function(e) {
+    $("#newQuote").on("click", function(e) {
         e.preventDefault();
-        getQuote();
-    });
+        corsRequest();
+    })
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
