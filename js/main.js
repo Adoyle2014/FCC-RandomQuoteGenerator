d7b15bed8139adfa05132e5085cca46f9d428f59
Adam Doyle <dominofoe@gmail.com>
2015-08-21 20:35:52 -0400

    It Finally Works

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 //Button click watcher and animation
     $("#newQuote").on('click', function() {
         $("#well").fadeOut(1000,function() {
-            makeCorsRequest();
+            apiCall();
         });
         $("#well").fadeIn(1000);
 
@@ -27,57 +27,32 @@ $(document).ready(function() {
         twttr.widgets.load();
     }
 
+    function apiCall() {
+        $.ajax({
+            url: "http://api.forismatic.com/api/1.0/",
+            jsonp: "jsonp",
+            dataType: "jsonp",
+            data: {
+                method: "getQuote",
+                lang: "en",
+                format: "jsonp"
+            },
+            success: function (response) {
+                parseQuote(response);
+            }
+        });
+    };
 
 
 
-// Create the XHR object.
-    function createCORSRequest(method, url) {
-        var xhr = new XMLHttpRequest();
-        if ("withCredentials" in xhr) {
-            // XHR for Chrome/Firefox/Opera/Safari.
-            xhr.open(method, url, true);
-        } else if (typeof XDomainRequest != "undefined") {
-            // XDomainRequest for IE.
-            xhr = new XDomainRequest();
-            xhr.open(method, url);
-        } else {
-            // CORS not supported.
-            xhr = null;
-        }
-        return xhr;
-    }
 
-
-// Make the actual CORS request.
-    function makeCorsRequest() {
-
-        var url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=xml';
-
-        var xhr = createCORSRequest('GET', url);
-        if (!xhr) {
-            alert('CORS not supported');
-            return;
-        }
-
-        // Response handlers.
-        xhr.onload = function() {
-            var text = xhr.responseText;
-            parseQuote(text);
-
-        };
-
-        xhr.onerror = function() {
-            alert('Oops, there was an error in your request! Please try again.');
-        };
-
-        xhr.send();
-    }
 
 
 // Place quotes and authors in HTML
     function parseQuote(response) {
         var quote = getQuote(response);
         var author = getAuthor(response);
+
         var wikiAuthor = author.replace(/-\s/, "");
         var wikiHref = 'https://en.wikipedia.org/wiki/' + wikiAuthor + '';
         var tweetQuote = quote + " -" + author;
@@ -91,11 +66,11 @@ $(document).ready(function() {
 
 //Helper functions to get data from XML response
     function getQuote(text) {
-        return text.match('<quoteText>(.*)?</quoteText>')[1];
+        return text.quoteText;
     }
 
     function getAuthor(text) {
-        var author = text.match('<quoteAuthor>(.*)?</quoteAuthor>')[1];
+        var author = text.quoteAuthor;
 
         if(author === undefined) {
             return author = "- Anonymous";
